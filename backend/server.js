70b2af06d3aff435b7401a649ceee75dca19c6e6
Ailany <88301756+ailanyEXE@users.noreply.github.com>
2022-06-03 14:19:30 -0400

    Deploy

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ mongoose.connect(process.env.MONGODB_URI,
   useNewUrlParser: true,
   useUnifiedTopology: true,
 }
-).then(() => console.log("MongoDB has been started..")).catch((err) => console.log(err))
+).then(() => console.log("MongoDB has been started.")).catch((err) => console.log(err))
 
 
 
